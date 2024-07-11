b6c14887f265990748d0e7be135facb0a65382d4
joao-vitor-l <b.loreti@ifsp.edu.br>
2024-07-11 18:16:25 -0300

    validação dos campos signin e signout

--- app/src/main/java/com/example/dmos5_projetofinal/SignInActivity.kt
+++ app/src/main/java/com/example/dmos5_projetofinal/SignInActivity.kt
@@ -1,6 +1,9 @@
 package com.example.dmos5_projetofinal
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -23,11 +26,31 @@ class SignInActivity : AppCompatActivity() {
 
     private fun signIn() {
         val prontuario = findViewById<EditText>(R.id.etProntuario).text.toString()
+
+        // Validação do campo
+        if (prontuario.length !=4) {
+            signalError(findViewById(R.id.tvHeader))
+            return
+        }
     }
 
     private fun signUp() {
         val intent = Intent(this, SignUpActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun signalError(textView: TextView) {
+        val animator = ObjectAnimator.ofObject(
+            textView,
+            "textColor",
+            ArgbEvaluator(),
+            textView.currentTextColor,
+            Color.parseColor("#ff4e5f")
+        )
+        animator.duration = 700
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.repeatCount = 1
+        animator.start()
     }
 
 }
--- app/src/main/java/com/example/dmos5_projetofinal/SignUpActivity.kt
+++ app/src/main/java/com/example/dmos5_projetofinal/SignUpActivity.kt
@@ -1,13 +1,13 @@
 package com.example.dmos5_projetofinal
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
 import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Html
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.RadioButton
+import android.widget.*
 
 import androidx.appcompat.app.AppCompatActivity
 
@@ -29,6 +29,12 @@ class SignUpActivity : AppCompatActivity() {
         val cpf = findViewById<EditText>(R.id.etCpf).text.toString()
         val cargo = if (findViewById<RadioButton>(R.id.rbGarcom).isChecked) Role.GARCOM else Role.COZINHEIRO
 
+        // Validação dos campos
+        if (nome.length < 3 || cpf.length != 11) {
+            signalError(findViewById(R.id.tvHeader))
+            return
+        }
+
         /* Gera o prontuário
         Atualizar método para que, ao invés de gerar um número aleatório de 4 dígitos,
         o primeiro prontuário comece em 1000 e seja incrementado a cada novo cadastro,
@@ -38,7 +44,7 @@ class SignUpActivity : AppCompatActivity() {
 
         // Pop-up com o prontuário
         AlertDialog.Builder(this)
-            .setTitle("Registro concluído")
+            .setTitle("Cadastro concluído")
             .setMessage(Html.fromHtml("Guarde seu prontuário: <b>$prontuario</b>"))
             .setPositiveButton("OK") { _, _ ->
                 // Registra o funcionário
@@ -53,6 +59,20 @@ class SignUpActivity : AppCompatActivity() {
     private fun returnToSignIn() {
         val intent = Intent(this, SignInActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun signalError(textView: TextView) {
+        val animator = ObjectAnimator.ofObject(
+            textView,
+            "textColor",
+            ArgbEvaluator(),
+            textView.currentTextColor,
+            Color.parseColor("#ff4e5f")
+        )
+        animator.duration = 700
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.repeatCount = 1
+        animator.start()
     }
 
 }
