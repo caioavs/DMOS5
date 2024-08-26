d3fda3a543d9d0f2709048098850902960bef3a0
joao-vitor-l <b.loreti@ifsp.edu.br>
2024-08-25 22:46:02 -0300

    botão p/ editar pedidos

--- app/src/main/java/com/example/dmos5_projetofinal/adapter/OrderAdapter.kt
+++ app/src/main/java/com/example/dmos5_projetofinal/adapter/OrderAdapter.kt
@@ -1,12 +1,16 @@
 package com.example.dmos5_projetofinal.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import com.example.dmos5_projetofinal.R
 import com.example.dmos5_projetofinal.model.Order
+import com.example.dmos5_projetofinal.ui.NewOrderActivity
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -16,6 +20,11 @@ class OrderAdapter(private val context: Context, private val orders: List<Order>
         val inflater = LayoutInflater.from(context)
         orders.forEach { order ->
             val itemView = inflater.inflate(R.layout.order_card, container, false)
+
+            editOrder(itemView)
+
+            setStatusLabel(itemView, order)
+
             bindOrder(itemView, order)
             container.addView(itemView)
         }
@@ -46,6 +55,25 @@ class OrderAdapter(private val context: Context, private val orders: List<Order>
         val dt = view.findViewById<TextView>(R.id.tvDt)
         val dtFormat = SimpleDateFormat("EEEE, dd/MM/yy HH:mm", Locale("pt", "BR"))
         dt.text = "${dt.text}${dtFormat.format(order.dt)}"
+    }
+
+    private fun editOrder(itemView: View) {
+        val cardView: CardView = itemView.findViewById(R.id.orderCard)
+        cardView.setOnClickListener {
+            val intent = Intent(context, NewOrderActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    private fun setStatusLabel(itemView: View, order: Order) {
+        val tvStatusLabel: TextView = itemView.findViewById(R.id.tvStatusLabel)
+        when(order.status) {
+            Order.Status.EM_ESPERA -> tvStatusLabel.setTextColor(ContextCompat.getColor(context, R.color.brown_lighter))
+            Order.Status.EM_ANDAMENTO -> tvStatusLabel.setTextColor(ContextCompat.getColor(context, R.color.blue_light))
+            Order.Status.PRONTO_PARA_RETIRADA -> tvStatusLabel.setTextColor(ContextCompat.getColor(context, R.color.yellow))
+            Order.Status.CONCLUIDO -> tvStatusLabel.setTextColor(ContextCompat.getColor(context, R.color.green))
+            else -> tvStatusLabel.setTextColor(ContextCompat.getColor(context, R.color.red))
+        }
     }
 
 }
